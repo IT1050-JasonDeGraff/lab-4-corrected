c0b09f5f99b544f208ccf80d6649e4783f4412d9
Jason Degraff <S00688209@acad.tri-c.edu>
2020-11-15 17:37:30 -0500

    Add project files.

---
+++ 0-100/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i<101; i++)
+            {
+                Console.WriteLine(i);
+                Console.WriteLine("*********");
+
+
+
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ odd and even plus questions/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace odd_and_even_plus_questions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Question 1
+             * Control Variable - loop counter 
+             * Initial Value - value of the control variable
+             * Increment or decrement variable - modifies the control variables value
+             * Loop-continuation condition - determines whether looping should continue
+             * 
+             * Question 2
+             * The both execute a statement or a block of statements while the Boolean expression is true. The difference is you use 
+             * the for loop when you know how many times the code needs to be looped and the while loop when you are unsure.
+             * 
+             * Question 3 
+             * If you want the do block to be executed at least once. 
+             * 
+             * 
+             */
+            int i = 1;
+            while (i <= 100)
+            {
+                Console.WriteLine(i);
+                i++;
+                {
+                    if ((i % 2) == 0)
+                    {
+                        Console.WriteLine("Number is odd");
+                    }
+                    else if ((i % 2) != 0)
+                    {
+                        Console.WriteLine("Number is even");
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
